5b96012eca946f78cea2c36324c39a3896b17b87
EC2 Default User <ec2-user@ip-10-136-91-7.ap-southeast-1.compute.internal>
2013-07-14 04:38:48 +0000

    Fixed encoding problem

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express'), fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('index.html',{encoding:'UTF8'});
+  var content = fs.readFileSync('index.html',{encoding:'utf8'});
   response.send(content);
 });
 
